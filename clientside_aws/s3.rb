6fbb79cdaac95af8a0ad610b2c454fb5096c8a73
Steve Tsourounis <steve.tsourounis@gmail.com>
2017-03-03 17:06:51 -0500

    S3 support for side-by-side usage

--- clientside_aws/s3.rb
+++ clientside_aws/s3.rb
@@ -2,20 +2,20 @@ require 'builder'
 
 helpers do
   def list_buckets
-    buckets = AWS_REDIS.keys "s3:bucket:*"
+    buckets = AWS_REDIS.keys 's3:bucket:*'
 
-    xml = Builder::XmlMarkup.new()
+    xml = Builder::XmlMarkup.new
     xml.instruct!
-    xml.ListAllMyBucketsResult(:xmlns => "http://doc.s3.amazonaws.com/2006-03-01") do
+    xml.ListAllMyBucketsResult(xmlns: 'http://s3.amazonaws.com/doc/2006-03-01') do
       xml.Owner do
         xml.tag!(:ID, UUID.new.generate)
-        xml.tag!(:DisplayName, "Fake Owner")
+        xml.tag!(:DisplayName, 'Fake Owner')
       end
       xml.Buckets do
         buckets.each do |bucket|
           xml.Bucket do
-            xml.tag!(:Name, bucket.split(":").last)
-            xml.tag!(:CreationDate, Time.at(AWS_REDIS.hget(bucket, "created_at").to_i).xmlschema)
+            xml.tag!(:Name, bucket.split(':').last)
+            xml.tag!(:CreationDate, Time.at(AWS_REDIS.hget(bucket, 'created_at').to_i).xmlschema)
           end
         end
       end
@@ -26,12 +26,10 @@ helpers do
   end
 
   def list_objects(bucket)
-
-    xml = Builder::XmlMarkup.new()
+    xml = Builder::XmlMarkup.new
     xml.instruct!
-    xml.ListBucketResult(:xmlns => "http://doc.s3.amazonaws.com/2006-03-01") do
+    xml.ListBucketResult(xmlns: 'http://doc.s3.amazonaws.com/2006-03-01') do
       objects = AWS_REDIS.keys "s3:bucket:#{bucket}:*"
-
       xml.tag!(:Name, bucket)
       xml.tag!(:KeyCount, objects.length)
       xml.tag!(:Prefix, nil)
@@ -45,19 +43,19 @@ helpers do
                     object.start_with?("s3:bucket:#{bucket}:#{prefix}")
 
         xml.Contents do
-          key = AWS_REDIS.hget object,  "key"
-          last_modified = AWS_REDIS.hget object, "last_modified"
-          etag = AWS_REDIS.hget object, "etag"
-          size = AWS_REDIS.hget object, "size"
+          key = AWS_REDIS.hget object, 'key'
+          last_modified = AWS_REDIS.hget object, 'last_modified'
+          etag = AWS_REDIS.hget object, 'etag'
+          size = AWS_REDIS.hget object, 'size'
 
           xml.tag!(:Key, key)
           xml.tag!(:LastModified, Time.at(last_modified.to_i).xmlschema)
           xml.tag!(:ETag, etag)
           xml.tag!(:Size, size)
-          xml.tag!(:Storage, "STANDARD")
+          xml.tag!(:Storage, 'STANDARD')
           xml.Owner do
             xml.tag!(:ID, UUID.new.generate)
-            xml.tag!(:DisplayName, "fake@example.com")
+            xml.tag!(:DisplayName, 'fake@example.com')
           end
         end
       end
@@ -68,75 +66,84 @@ helpers do
   end
 
   def objectNotFound
-    xml = Builder::XmlMarkup.new()
+    xml = Builder::XmlMarkup.new
     xml.instruct!
     xml.Error do
-      xml.tag!(:Code, "NoSuchKey")
-      xml.tag!(:Message, "The specified key does not exist.")
+      xml.tag!(:Code, 'NoSuchKey')
+      xml.tag!(:Message, 'The specified key does not exist.')
     end
     content_type :xml
     xml.target!
   end
 
-
-  def downloadFile(bucket, obj_name)
+  def download_file(bucket, obj_name)
     obj_key = "s3:bucket:#{bucket}:#{obj_name}"
-    return (AWS_REDIS.hget obj_key, 'body').force_encoding("UTF-8")
+    (AWS_REDIS.hget obj_key, 'body').force_encoding('UTF-8')
   end
-
 end
 
-get %r{/s3(.*?\.amazonaws\.com)?/(.+?)/(.+)} do
-  bucket = params[:captures][1]
+# Get all of the buckets
+#   Host: s3[.-][us-mockregion-1].amazonaws.com
+get %r{/s3.*?\.amazonaws\.com/?} do
+  list_buckets
+
+  status 200
+end
+
+# Get bucket
+#   Host: bucket-name.s3[.-][us-mockregion-1].amazonaws.com
+get %r{/(.*?)\.(s3.*?\.amazonaws\.com)/?$} do
+  bucket = params[:captures][0]
+  halt 404 unless AWS_REDIS.exists "s3:bucket:#{bucket}"
+  list_objects(bucket)
+end
+
+# Get a file a bucket
+#   Host: bucket-name.s3[.-][us-mockregion-1].amazonaws.com/file-name
+get %r{/(.*?)\.(s3.*?\.amazonaws\.com)?/(.+)} do
+  bucket = params[:captures][0]
   file_name = params[:captures][2]
 
-  halt 404, objectNotFound if AWS_REDIS.hget("s3:bucket:#{bucket}:#{file_name}", "body").nil?
+  halt 404, objectNotFound if AWS_REDIS.hget("s3:bucket:#{bucket}:#{file_name}", 'body').nil?
 
-  body = downloadFile(bucket, file_name)
-  content_type = AWS_REDIS.hget("s3:bucket:#{bucket}:#{file_name}", "content-type")
-  response.headers["Content-Type"] = content_type.nil? ? 'html' : content_type
+  body = download_file(bucket, file_name)
+  content_type = AWS_REDIS.hget("s3:bucket:#{bucket}:#{file_name}", 'content-type')
+  response.headers['content-type'] = content_type.nil? ? 'html' : content_type
   # response.headers["Content-Length"] = body.length.to_s
-  response.headers["ETag"] = Digest::MD5.hexdigest(body)
+  response.headers['etag'] = Digest::MD5.hexdigest(body)
   response.body = body
 
   status 200
 end
 
-get %r{^/s3(.*?\.amazonaws\.com)?/(.+?)/?$} do
-  bucket = params[:captures][1]
-  list_objects(bucket)
-end
+# Bucket creation
+#   Host: bucket-name.s3[.-][us-mockregion-1].amazonaws.com
+put %r{/(.*?)\.(s3\.?.*?\.amazonaws\.com)/?$} do
+  bucket = params[:captures][0]
+  AWS_REDIS.hset "s3:bucket:#{bucket}", 'created_at', Time.now.to_i
 
-get %r{^/s3(.*?\.amazonaws\.com)?/$} do
-  bucket = params[:captures][1]
-  list_buckets
   status 200
 end
 
-delete %r{/s3(.*?\.amazonaws\.com)?/(.+?)/(.+)} do
-  bucket = params[:captures][1]
-  file_name = params[:captures][2]
-  AWS_REDIS.del "s3:bucket:#{bucket}:#{file_name}"
-  status 200
-end
-
-put %r{/s3(.*?\.amazonaws\.com)?/(.+?)/(.+)} do
-  bucket = params[:captures][1]
+# Upload file into a bucket
+#   Host: bucket-name.s3[.-][us-mockregion-1].amazonaws.com/file-name
+put %r{/(.*?)\.s3\.(.*?\.amazonaws\.com)?/(.+)} do
+  bucket = params[:captures][0]
   file_name = params[:captures][2]
 
   # upload the file (chunking not implemented) to fake S3
-  if file_name and bucket
+  if file_name && bucket
     file_name = file_name[1..-1] if '/' == file_name[0]
     body_send = AWS::Core.testing ? params[:body] : request.body.read
     # Handle the copy_XXX case
-    if ((body_send.nil? or body_send.length == 0) and (env.has_key?("HTTP_X_AMZ_COPY_SOURCE") or env.has_key?("x-amz-copy-source")))
-      copy_source = env["HTTP_X_AMZ_COPY_SOURCE"] || env["x-amz-copy-source"]
-      if copy_source.start_with?("/")
-        (extra, srcbucket, srcfile) = copy_source.split("/")
+    if (body_send.nil? || body_send.empty?) && (env.key?('HTTP_X_AMZ_COPY_SOURCE') || env.key?('x-amz-copy-source'))
+      copy_source = env['HTTP_X_AMZ_COPY_SOURCE'] || env['x-amz-copy-source']
+      if copy_source.start_with?('/')
+        (_extra, srcbucket, srcfile) = copy_source.split('/')
       else
-        (srcbucket, srcfile) = copy_source.split("/")
+        (srcbucket, srcfile) = copy_source.split('/')
       end
-      body_send = downloadFile(srcbucket, srcfile)
+      body_send = download_file(srcbucket, srcfile)
     end
 
     AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", 'body', body_send
@@ -145,37 +152,43 @@ put %r{/s3(.*?\.amazonaws\.com)?/(.+?)/(.+)} do
     AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", 'size', body_send.length
     AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", 'etag', Digest::MD5.hexdigest(body_send)
 
-    if env.has_key?('content-type')
-      AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", "content-type", env['content-type']
-    elsif env.has_key?('CONTENT_TYPE')
-      AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", "content-type", env['CONTENT_TYPE']
+    %w(content-type Content-Type CONTENT_TYPE).each do |content_type_key|
+      next unless env.key?(content_type_key)
+      AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}",
+                     'content-type',
+                     env[content_type_key]
+      break
     end
   end
+
   status 200
 end
 
-put %r{/s3(.*?\.amazonaws\.com)?/([^/]+?)/?$} do
-  # bucket creation
-  bucket = params[:captures][1]
-  AWS_REDIS.hset "s3:bucket:#{bucket}", "created_at", Time.now.to_i
+# Delete file from a bucket
+#   Host: bucket-name.s3[.-][us-mockregion-1].amazonaws.com/file-name
+delete %r{/(.*?)\.s3\.(.*?\.amazonaws\.com)?/(.+)} do
+  bucket = params[:captures][0]
+  file_name = params[:captures][2]
+  AWS_REDIS.del "s3:bucket:#{bucket}:#{file_name}"
+
   status 200
 end
 
-post %r{/s3(.*?\.amazonaws\.com)?/([^/]+)/?} do
- # upload the file (chunking not implemented) to fake S3
- bucket = params[:captures][1]
- file_name = params[:key]
-  if file_name
-    file_name = file_name[1..-1] if file_name.start_with? '/'
-    body_send = params[:file]
-    body_send = body_send[:tempfile].read unless AWS::Core.testing
-
-   AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", "body", body_send
-    if env.has_key?('content-type')
-      AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", "content-type", env['content-type']
-    elsif env.has_key?('CONTENT_TYPE')
-      AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", "content-type", env['CONTENT_TYPE']
-    end
-  end
-  status 200
-end
+# post %r{/s3(.*?\.amazonaws\.com)?/([^/]+)/?} do
+#   # upload the file (chunking not implemented) to fake S3
+#   bucket = params[:captures][1]
+#   file_name = params[:key]
+#   if file_name
+#     file_name = file_name[1..-1] if file_name.start_with? '/'
+#     body_send = params[:file]
+#     body_send = body_send[:tempfile].read unless AWS::Core.testing
+#
+#     AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", 'body', body_send
+#     if env.key?('content-type')
+#       AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", 'content-type', env['content-type']
+#     elsif env.key?('CONTENT_TYPE')
+#       AWS_REDIS.hset "s3:bucket:#{bucket}:#{file_name}", 'content-type', env['CONTENT_TYPE']
+#     end
+#   end
+#   status 200
+# end
--- spec/s3_spec.rb
+++ spec/s3_spec.rb
@@ -2,6 +2,7 @@ $LOAD_PATH << "#{File.dirname(__FILE__)}/../"
 
 require 'spec/spec_helper'
 require 'aws-sdk'
+require 'aws-sdk-v1'
 require 'aws_mock'
 
 describe 'Profiles Spec' do
@@ -11,106 +12,210 @@ describe 'Profiles Spec' do
   end
   # adding in for ability to access via other 'examples'
 
-  it "says hello" do
+  it 'says hello' do
     get '/'
     expect(last_response.ok?).to be true
   end
 
-  it "should post to S3 okay" do
-    s3 = AWS::S3.new(
-      :access_key_id => "...",
-      :secret_access_key => "...")
+  it 'v1: should post to S3 okay' do
+    s3 = AWS::S3.new
     s3.buckets.create('test')
-    bucket = s3.buckets[:test]
-    bucket.exists?
+    bucket = s3.buckets['test']
+    expect(bucket.exists?).to be true
     object = bucket.objects['test.file']
 
-    initial_hash = Digest::MD5.hexdigest(File.read("#{File.dirname(__FILE__)}/../public/images/spacer.gif"))
-    object.write(:file => "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
-    expect(Digest::MD5.hexdigest(object.read())).to eq initial_hash
+    initial_hash = \
+      Digest::MD5.hexdigest(File.read("#{File.dirname(__FILE__)}" \
+                                      '/../public/images/spacer.gif'))
+
+    object.write(file: "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
+    expect(Digest::MD5.hexdigest(object.read)).to eq initial_hash
     expect(object.exists?).to be true
 
     expect(bucket.objects['asdfdsfasdf'].exists?).to be false
 
-    object.delete()
+    object.delete
     expect(object.exists?).to be false
 
-    json_value = {:foo => "bar"}.to_json
+    json_value = { foo: 'bar' }.to_json
     object = bucket.objects['test.json']
-    object.write(json_value, :content_type => "application/json")
-    expect(object.read()).to eq json_value
-    expect(object.content_type).to eq "application/json"
+    object.write(json_value, content_type: 'application/json')
+    expect(object.read).to eq json_value
+    expect(object.content_type).to eq 'application/json'
     expect(object.etag).to eq Digest::MD5.hexdigest(json_value)
   end
 
-  it "should support subpaths" do
-    s3 = AWS::S3.new(
-      :access_key_id => "...",
-      :secret_access_key => "...")
+  it 'v2: should post to S3 okay' do
+    s3 = Aws::S3::Client.new
+    s3.create_bucket(bucket: 'test')
+
+    bucket = Aws::S3::Resource.new.bucket('test')
+    expect(bucket.exists?).to be true
+
+    s3.put_object(bucket: 'test',
+                  key: 'test.file',
+                  body: File.new("#{File.dirname(__FILE__)}" \
+                        '/../public/images/spacer.gif'))
+    initial_hash = \
+      Digest::MD5.hexdigest(File.read("#{File.dirname(__FILE__)}" \
+                                      '/../public/images/spacer.gif'))
+
+    bucket = Aws::S3::Resource.new.bucket('test')
+    object = bucket.object('test.file')
+    expect(Digest::MD5.hexdigest(object.get.body.read)).to eq initial_hash
+
+    expect(object.exists?).to be true
+
+    # expect(bucket.objects['asdfdsfasdf'].exists?).to be false
+
+    object.delete
+    object = bucket.object('test.file')
+
+    expect(object.exists?).to be false
+  end
+
+  it 'v2: stores JSON document' do
+    s3 = Aws::S3::Client.new
+    s3.create_bucket(bucket: 'test')
+
+    bucket = Aws::S3::Resource.new.bucket('test')
+    expect(bucket.exists?).to be true
+
+    # Now, store a JSON document
+    json_value = { foo: 'bar' }.to_json
+    object = bucket.object('test.json')
+    object.put(body: json_value, content_type: 'application/json')
+
+    object = bucket.object('test.json')
+    expect(object.get.body.read).to eq json_value
+    expect(object.content_type).to eq 'application/json'
+    expect(object.etag).to eq Digest::MD5.hexdigest(json_value)
+  end
+
+  it 'v1: should support subpaths' do
+    s3 = AWS::S3.new
     s3.buckets.create('test')
     bucket = s3.buckets[:test]
     expect(bucket.exists?).to be true
     object = bucket.objects['foo/bar/test.file']
 
-    object.write(:file => "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
+    object.write(file: "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
     expect(object.exists?).to be true
 
-    get '/s3/test/foo/bar/test.file'
-    expect(last_response.ok?).to be true
-    expect(last_response.status).to eq 200
-
+    s3_object = object.read
+    expect(s3_object.length).to eq 43
   end
 
-  it "should support rename_to" do
-    s3 = AWS::S3.new(
-      :access_key_id => "...",
-      :secret_access_key => "...")
+  it 'v2: should support subpaths' do
+    s3 = Aws::S3::Client.new
+
+    s3.create_bucket(bucket: 'test')
+    bucket = Aws::S3::Resource.new.bucket('test')
+    expect(bucket.exists?).to be true
+    object = bucket.object('foo/bar/test.file')
+
+    object.put(body: File.new("#{File.dirname(__FILE__)}" \
+                              '/../public/images/spacer.gif'))
+    expect(object.exists?).to be true
+
+    gif_content = object.get.body.read
+    expect(gif_content.length).to eq 43
+  end
+
+  it 'v1: should support rename_to' do
+    s3 = AWS::S3.new
     s3.buckets.create('test1')
     bucket = s3.buckets[:test]
     expect(bucket.exists?).to be true
-    object = bucket.objects['test.file']
+    object = bucket.objects['test1_v1.file']
 
     initial_hash = Digest::MD5.hexdigest(File.read("#{File.dirname(__FILE__)}/../public/images/spacer.gif"))
-    object.write(:file => "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
-    expect(Digest::MD5.hexdigest(object.read())).to eq initial_hash
+    object.write(file: "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
+    expect(Digest::MD5.hexdigest(object.read)).to eq initial_hash
     expect(object.exists?).to be true
 
-    object.rename_to("test2.file")
+    object.rename_to('test2_v1.file')
 
-    renamed_object = bucket.objects['test2.file']
+    renamed_object = bucket.objects['test2_v1.file']
     expect(renamed_object.exists?).to be true
-
   end
 
-  it 'should list' do
-    s3 = AWS::S3.new(
-      :access_key_id => "...",
-      :secret_access_key => "...")
+  it 'v2: should support rename_to' do
+    s3 = Aws::S3::Client.new
+
+    s3.create_bucket(bucket: 'test')
+    bucket = Aws::S3::Resource.new.bucket('test')
+    expect(bucket.exists?).to be true
+
+    object = bucket.object('test_v2.file')
+    object.put(body: File.new("#{File.dirname(__FILE__)}" \
+                              '/../public/images/spacer.gif'))
+
+    object2 = bucket.object('test2_v2.file')
+    expect(object2.exists?).to be false
+    object.move_to(object2)
+    expect(object2.exists?).to be true
+    expect(object.exists?).to be false
+  end
+
+  it 'v1: should list' do
+    s3 = AWS::S3.new
     bucket_name = UUID.new.generate
     s3.buckets.create(bucket_name)
     bucket = s3.buckets[bucket_name]
     expect(bucket.exists?).to be true
     object = bucket.objects['test.file']
-    object.write(:file => "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
+    object.write(file: "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
 
     expect(bucket.objects.count).to eq 1
   end
 
-  it 'should respect prefix' do
-    s3 = AWS::S3.new(
-      :access_key_id => "...",
-      :secret_access_key => "...")
+  it 'v2: should list' do
+    s3 = Aws::S3::Client.new
+
+    bucket_name = UUID.new.generate
+    s3.create_bucket(bucket: bucket_name)
+    bucket = Aws::S3::Resource.new.bucket(bucket_name)
+    expect(bucket.exists?).to be true
+
+    object = bucket.object('test.file')
+    object.put(body: File.new("#{File.dirname(__FILE__)}" \
+                              '/../public/images/spacer.gif'))
+
+    expect(bucket.objects.count).to eq 1
+  end
+
+  it 'v1: should respect prefix' do
+    s3 = AWS::S3.new
     bucket_name = UUID.new.generate
     s3.buckets.create(bucket_name)
     bucket = s3.buckets[bucket_name]
     expect(bucket.exists?).to be true
     object = bucket.objects['apple.gif']
-    object.write(:file => "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
+    object.write(file: "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
 
     object = bucket.objects['banana.gif']
-    object.write(:file => "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
+    object.write(file: "#{File.dirname(__FILE__)}/../public/images/spacer.gif")
 
     expect(bucket.objects.with_prefix('apple').count).to eq 1
   end
 
+  it 'v2: should respect prefix' do
+    s3 = Aws::S3::Client.new
+
+    bucket_name = UUID.new.generate
+    s3.create_bucket(bucket: bucket_name)
+    bucket = Aws::S3::Resource.new.bucket(bucket_name)
+    expect(bucket.exists?).to be true
+
+    object = bucket.object('apple.gif')
+    object.put(body: File.new("#{File.dirname(__FILE__)}" \
+                              '/../public/images/spacer.gif'))
+
+    object = bucket.object('banana.gif')
+    object.put(body: File.new("#{File.dirname(__FILE__)}" \
+                              '/../public/images/spacer.gif'))
+
+    expect(bucket.objects(prefix: 'apple').count).to eq 1
+  end
 end
