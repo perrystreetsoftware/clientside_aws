6f7ad5e9fee09369f459a58c19f6c2c6a99483bd
Steve Tsourounis <steve.tsourounis@gmail.com>
2017-03-03 17:06:04 -0500

    Kinesis support for side-by-side usage

--- clientside_aws/kinesis.rb
+++ clientside_aws/kinesis.rb
@@ -1,11 +1,13 @@
-post %r{/kinesis(\.(\w+?)\.amazonaws\.com)?/(.*)} do
-  if env["REQUEST_METHOD"] == "POST"
-    args = JSON::parse(env['rack.input'].read)
-  else
-    args = env['rack.request.form_hash']
-  end
+post %r{/kinesis(\.(\w+?)\.amazonaws\.com)?/?(.*)} do
+  args = if env['REQUEST_METHOD'] == 'POST'
+           JSON.parse(env['rack.input'].read)
+         else
+           env['rack.request.form_hash']
+         end
 
-  AWS_REDIS.zadd "kinesis:#{args['StreamName']}", Time.now.to_i, Base64.decode64(args['Data'])
+  AWS_REDIS.zadd "kinesis:#{args['StreamName']}",
+                 Time.now.to_i,
+                 Base64.decode64(args['Data'])
 
   200
 end
---
+++ spec/kinesis_spec.rb
@@ -0,0 +1,26 @@
+
+$LOAD_PATH << "#{File.dirname(__FILE__)}/../"
+
+require 'spec/spec_helper'
+require 'aws-sdk'
+require 'aws-sdk-v1'
+require 'aws_mock'
+
+describe 'Profiles Spec' do
+  include Rack::Test::Methods
+  def app
+    Sinatra::Application
+  end
+
+  it 'v1: should post to Kinesis okay' do
+    AWS::Kinesis::Client.new.put_record(stream_name: 'foo',
+                                        data: { bar: 1 }.to_json,
+                                        partition_key: 1.to_s)
+  end
+
+  it 'v2: should post to Kinesis okay' do
+    Aws::Kinesis::Client.new.put_record(stream_name: 'foo',
+                                        data: { bar: 1 }.to_json,
+                                        partition_key: 1.to_s)
+  end
+end
