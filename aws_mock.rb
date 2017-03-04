3d91a3f6e784102792afde17ded317326f36f221
Steve Tsourounis <steve.tsourounis@gmail.com>
2017-03-03 20:49:35 -0500

    Update aws_mock to within and outside of rspec

--- aws_mock.rb
+++ aws_mock.rb
@@ -1,6 +1,6 @@
 require_relative 'clientside_aws/core'
 require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'aws')
 
 # WebMock.before_request do |request_signature, response|
 #   puts "Request #{request_signature} was made and #{response} was returned"
@@ -26,56 +26,120 @@ module AWS
   end
 end
 
+def mock_uri(uri:)
+  uri.scheme = 'http'
+  uri.path = uri.host + uri.path
+  uri.host = 'aws'
+  uri.port = '4567'
+
+  uri
+end
+
+def mock_post(request:)
+  response = HTTParty.post(mock_uri(uri: request.uri),
+                           body: request.body,
+                           headers: \
+                            request.headers.merge('SERVER_NAME' => \
+                                                  request.uri.host))
+  { headers: response.headers,
+    status: response.code,
+    body: response.body }
+end
+
+def mock_get(request:)
+  response = HTTParty.get(mock_uri(uri: request.uri),
+                          query: request.uri.query_values,
+                          headers: \
+                            request.headers.merge('SERVER_NAME' => \
+                                                  request.uri.host))
+
+  { headers: response.headers,
+    status: response.code,
+    body: response.body }
+end
+
+def mock_head(request:)
+  response = HTTParty.get(mock_uri(uri: request.uri),
+                          query: request.uri.query_values,
+                          headers: \
+                            request.headers.merge('SERVER_NAME' => \
+                                                  request.uri.host))
+
+  { headers: response.headers,
+    status: response.code,
+    body: '' }
+end
+
+def mock_put(request:)
+  response = HTTParty.put(mock_uri(uri: request.uri),
+                          body: request.body,
+                          headers: \
+                            request.headers.merge('SERVER_NAME' => \
+                                                  request.uri.host))
+
+  { headers: response.headers,
+    status: response.code,
+    body: response.body }
+end
+
+def mock_delete(request:)
+  response = HTTParty.delete(mock_uri(uri: request.uri),
+                             query: request.uri.query_values,
+                             headers: \
+                               request.headers.merge('SERVER_NAME' => \
+                                                     request.uri.host))
+
+  { headers: response.headers,
+    status: response.code,
+    body: response.body }
+end
+
+WebMock.stub_request(:post, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+       .to_return do |request|
+  mock_post(request: request)
+end
+
+WebMock.stub_request(:get, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+       .to_return do |request|
+  mock_get(request: request)
+end
+
+WebMock.stub_request(:head, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+       .to_return do |request|
+  mock_head(request: request)
+end
+
+WebMock.stub_request(:put, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+       .to_return do |request|
+  mock_put(request: request)
+end
+
+WebMock.stub_request(:delete, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+       .to_return do |request|
+  mock_delete(request: request)
+end
+
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:post, /us-mockregion-1/).to_return do |request|
-      post "/#{request.uri.host}",
-           request.body,
-           request.headers.merge('SERVER_NAME' => request.uri.host)
-
-      { headers: last_response.header,
-        status: last_response.status,
-        body: last_response.body }
+    stub_request(:post, %r{https\:\/\/([\w\.]+\.us\-mockregion\-1|aws)}) \
+      .to_return do |request|
+      mock_post(request: request)
     end
-
-    stub_request(:get, /us-mockregion-1/).to_return do |request|
-      get "/#{request.uri.host}#{request.uri.path}",
-          request.uri.query_values,
-          request.headers.merge('SERVER_NAME' => request.uri.host)
-
-      { headers: last_response.header,
-        status: last_response.status,
-        body: last_response.body }
+    stub_request(:get, %r{https\:\/\/([\w\.]+\.us\-mockregion\-1|aws)}) \
+      .to_return do |request|
+      mock_get(request: request)
     end
-
-    stub_request(:put, /us-mockregion-1/).to_return do |request|
-      put "/#{request.uri.host}#{request.uri.path}",
-          { body: request.body },
-          request.headers.merge('SERVER_NAME' => request.uri.host)
-
-      { headers: last_response.header,
-        status: last_response.status,
-        body: last_response.body }
+    stub_request(:head, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+      .to_return do |request|
+      mock_head(request: request)
     end
-
-    stub_request(:head, /us-mockregion-1/).to_return do |request|
-      get "/#{request.uri.host}#{request.uri.path}",
-          { head_request: 1 }.merge(request.uri.query_values || {}),
-          request.headers.merge('SERVER_NAME' => request.uri.host)
-
-      { headers: last_response.header,
-        status: last_response.status,
-        body: '' }
+    stub_request(:put, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+      .to_return do |request|
+      mock_put(request: request)
     end
-
-    stub_request(:delete, /us-mockregion-1/).to_return do |request|
-      delete "/#{request.uri.host}#{request.uri.path}",
-             request.uri.query_values,
-             request.headers.merge('SERVER_NAME' => request.uri.host)
-
-      { headers: last_response.header,
-        status: last_response.status,
-        body: last_response.body }
+    stub_request(:delete, %r{https\:\/\/[\w\.]+\.us\-mockregion\-1}) \
+      .to_return do |request|
+      mock_delete(request: request)
     end
   end
 end
